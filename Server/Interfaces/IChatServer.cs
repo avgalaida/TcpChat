7a44a0298271d8ef6fa72ee13ed775e6282dbd36
Alexandr Galaida <evaAxelrod@yandex.ru>
2024-11-02 14:33:55 +0500

    feat/fix: enhanced IChatServer

--- Server/Interfaces/IChatServer.cs
+++ Server/Interfaces/IChatServer.cs
@@ -3,8 +3,7 @@ namespace Server.Interfaces;
 public interface IChatServer
 {
     Task StartAsync();
-
-    void BroadcastMessage(string message, string sender);
-
+    void AddClient(IClientHandler client);
     void RemoveClient(IClientHandler client);
+    Task BroadcastMessageAsync(string message, string sender);
 }
