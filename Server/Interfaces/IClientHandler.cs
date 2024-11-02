00fde3b3ea533e8d877f82bd0c500c5afe9c5ead
Alexandr Galaida <evaAxelrod@yandex.ru>
2024-11-02 14:32:15 +0500

    refactor: reordered fields

--- Server/Interfaces/IClientHandler.cs
+++ Server/Interfaces/IClientHandler.cs
@@ -2,11 +2,8 @@ namespace Server.Interfaces;
 
 public interface IClientHandler
 {
-    Task ProcessAsync();
-
-    Task SendMessageAsync(string message);
-
-    void Disconnect();
-
     string ClientId { get; }
+    Task ProcessAsync();
+    Task SendMessageAsync(string message);
+    void Disconnect();
 }
