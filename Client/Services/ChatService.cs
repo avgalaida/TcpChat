977b0956dfe68d0dc91b0096e0b9c5467b9e5470
avgalaida <evaAxelrod@yandex.ru>
2024-11-03 19:01:17 +0500

    feat: implement ChatService

--- Client/Services/ChatService.cs
+++ Client/Services/ChatService.cs
@@ -1,5 +1,6 @@
 ﻿using Client.Models;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Client.Services;
 
@@ -20,16 +21,97 @@ public class ChatService : IChatService
 
     public void Connect()
     {
-        throw new NotImplementedException();
+        _tcpClient = new TcpClient();
+        _tcpClient.Connect(_serverIp, _serverPort);
+        _stream = _tcpClient.GetStream();
+
+        Task.Run(ReceiveMessagesAsync);
     }
 
     public void Disconnect()
     {
-        throw new NotImplementedException();
+        _stream?.Close();
+        _tcpClient?.Close();
     }
 
-    public Task SendMessageAsync(string message)
+    public async Task SendMessageAsync(string message)
     {
-        throw new NotImplementedException();
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var lengthPrefix = BitConverter.GetBytes(messageBytes.Length);
+
+        await _stream.WriteAsync(lengthPrefix, 0, lengthPrefix.Length);
+        await _stream.WriteAsync(messageBytes, 0, messageBytes.Length);
     }
+
+    private async Task ReceiveMessagesAsync()
+    {
+        try
+        {
+            while (true)
+            {
+                var message = await ReadMessageAsync();
+                if (message == null)
+                {
+                    break;
+                }
+
+                var chatMessage = new ChatMessage
+                {
+                    Sender = ExtractSender(message),
+                    Content = ExtractContent(message),
+                    Timestamp = DateTime.Now
+                };
+
+                MessageReceived?.Invoke(this, chatMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            // TODO
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    private async Task<string> ReadMessageAsync()
+    {
+        var lengthBuffer = new byte[4];
+        var bytesRead = await _stream.ReadAsync(lengthBuffer, 0, lengthBuffer.Length);
+        if (bytesRead == 0)
+        {
+            return null;
+        }
+
+        var messageLength = BitConverter.ToInt32(lengthBuffer, 0);
+
+        var messageBuffer = new byte[messageLength];
+        int totalBytesRead = 0;
+        while (totalBytesRead < messageLength)
+        {
+            bytesRead = await _stream.ReadAsync(messageBuffer, totalBytesRead, messageLength - totalBytesRead);
+            if (bytesRead == 0)
+            {
+                return null;
+            }
+            totalBytesRead += bytesRead;
+        }
+
+        var message = Encoding.UTF8.GetString(messageBuffer);
+        return message;
+    }
+
+    private string ExtractSender(string message)
+    {
+        var parts = message.Split(new[] { ':' }, 2);
+        return parts.Length > 1 ? parts[0] : "Неизвестно";
+    }
+
+    private string ExtractContent(string message)
+    {
+        var parts = message.Split(new[] { ':' }, 2);
+        return parts.Length > 1 ? parts[1].Trim() : message;
+    }
+
 }
