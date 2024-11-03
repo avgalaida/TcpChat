118f791bcb052ce7d50e0c015daea2377cf288a1
avgalaida <evaAxelrod@yandex.ru>
2024-11-03 18:09:30 +0500

    feat: add ChatMessage model

---
+++ Client/Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Models;
+
+public class ChatMessage
+{
+    public string Sender { get; set; }
+    public string Content { get; set; }
+    public DateTime Timestamp { get; set; }
+}
+
