28e4cab9dcc76642069553129bdf8b070c14b8d9
avgalaida <evaAxelrod@yandex.ru>
2024-11-03 18:45:35 +0500

    feat: create MainViewModel with properties and commands

---
+++ Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Client.Models;
+using Client.Services;
+using Client.Utilities;
+
+namespace Client.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private readonly IChatService _chatService;
+
+    public ObservableCollection<ChatMessage> Messages { get; } = new ObservableCollection<ChatMessage>();
+
+    private string _newMessage;
+    public string NewMessage
+    {
+        get => _newMessage;
+        set => SetProperty(ref _newMessage, value);
+    }
+
+    public ICommand SendMessageCommand { get; }
+
+    public MainViewModel(IChatService chatService)
+    {
+        _chatService = chatService;
+
+        SendMessageCommand = new RelayCommand(async () => await SendMessageAsync(), CanSendMessage);
+
+        // TODO: MessageReceived event subscription
+    }
+
+    private async Task SendMessageAsync()
+    {
+        if (!string.IsNullOrWhiteSpace(NewMessage))
+        {
+            await _chatService.SendMessageAsync(NewMessage);
+            NewMessage = string.Empty;
+        }
+    }
+
+    private bool CanSendMessage()
+    {
+        return !string.IsNullOrWhiteSpace(NewMessage);
+    }
+}
