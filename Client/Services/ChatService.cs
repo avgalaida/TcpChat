b2287ed59e76c5bd2e47110382a77dbb32c35666
avgalaida <evaAxelrod@yandex.ru>
2024-11-03 18:43:28 +0500

    feat: implement IChatService

---
+++ Client/Services/ChatService.cs
@@ -0,0 +1,35 @@
+﻿using Client.Models;
+using System.Net.Sockets;
+
+namespace Client.Services;
+
+public class ChatService : IChatService
+{
+    public event EventHandler<ChatMessage> MessageReceived;
+
+    private TcpClient _tcpClient;
+    private NetworkStream _stream;
+    private readonly string _serverIp;
+    private readonly int _serverPort;
+
+    public ChatService(string serverIp, int serverPort)
+    {
+        _serverIp = serverIp;
+        _serverPort = serverPort;
+    }
+
+    public void Connect()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Disconnect()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendMessageAsync(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Client/Services/IChatService.cs
@@ -0,0 +1,11 @@
+﻿using Client.Models;
+
+namespace Client.Services;
+public interface IChatService
+{
+    event EventHandler<ChatMessage> MessageReceived;
+
+    void Connect();
+    Task SendMessageAsync(string message);
+    void Disconnect();
+}
