f45bd8efd0690fa91da44ae0f7f88c535a8e51a0
avgalaida <evaAxelrod@yandex.ru>
2024-11-03 18:47:10 +0500

    feat: configure DI and set DataContext for MainWindow

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Client.Services;
+using Client.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
-namespace Client
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace Client;
 
+public partial class App : Application
+{
+    public new static App Current => (App)Application.Current;
+
+    public IServiceProvider ServiceProvider { get; private set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<IChatService>(provider => new ChatService("127.0.0.1", 3333));
+        services.AddTransient<MainViewModel>();
+
+        ServiceProvider = services.BuildServiceProvider();
+
+        var mainWindow = new Views.MainWindow
+        {
+            DataContext = ServiceProvider.GetRequiredService<MainViewModel>()
+        };
+        mainWindow.Show();
+    }
 }
+
+
