fb42b34ebc5004f5d0da1f6422da4e410d8b991f
avgalaida <evaAxelrod@yandex.ru>
2024-11-04 14:51:48 +0500

    feat: add various UI improvements and enhancements

--- Client/Views/MainWindow.xaml.cs
+++ Client/Views/MainWindow.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using Client.ViewModels;
 using System.Collections.Specialized;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Client.Views;
-
 public partial class MainWindow : Window
 {
     public MainWindow()
@@ -16,6 +16,8 @@ public partial class MainWindow : Window
             var messages = viewModel.Messages;
             messages.CollectionChanged += Messages_CollectionChanged;
         }
+
+        MessageTextBox.KeyDown += MessageTextBox_KeyDown;
     }
 
     private void Messages_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -23,6 +25,19 @@ public partial class MainWindow : Window
         if (e.Action == NotifyCollectionChangedAction.Add)
         {
             MessagesListBox.ScrollIntoView(MessagesListBox.Items[MessagesListBox.Items.Count - 1]);
+        }
+    }
+
+    private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var viewModel = DataContext as MainViewModel;
+            if (viewModel?.SendMessageCommand?.CanExecute(null) == true)
+            {
+                viewModel.SendMessageCommand.Execute(null);
+                e.Handled = true; 
+            }
         }
     }
 }
