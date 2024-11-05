621d21baac0ef41a2afc167063dd07f0b01b24b1
Alexandr Galaida <evaAxelrod@yandex.ru>
2024-11-05 19:53:21 +0500

    feat: Add IP and Port fields to ChatMessage model

--- Server/Migrations/20241105145125_add-ip-port-messageModel.Designer.cs
+++ Server/Migrations/20241105145125_add-ip-port-messageModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Data;
 
@@ -10,9 +11,10 @@ using Server.Data;
 namespace Server.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241105145125_add-ip-port-messageModel")]
+    partial class addipportmessageModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.35");
