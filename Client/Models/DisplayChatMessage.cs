53e98ee276016f711c46737ce52d5ca95164cc40
avgalaida <evaAxelrod@yandex.ru>
2024-11-08 13:06:37 +0500

    refactor: update Messages model

--- Client/Models/DisplayChatMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Client.Models;
-
-public class DisplayChatMessage
-{
-    public string Sender { get; set; }
-    public string Content { get; set; }
-    public DateTime Timestamp { get; set; }
-    public bool IsSentByUser { get; set; }
-}
--- Client/Models/Messages/IncomingChatMessage.cs
+++ Client/Models/Messages/IncomingChatMessage.cs
@@ -2,7 +2,10 @@ namespace Client.Models.Messages;
 
 public class IncomingChatMessage : BaseMessage
 {
+    public Guid Id { get; set; }
     public string Sender { get; set; }
     public string Content { get; set; }
     public DateTime Timestamp { get; set; }
+    public string SenderIp { get; set; }
+    public int SenderPort { get; set; }
 }
