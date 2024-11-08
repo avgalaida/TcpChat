9ed1021edc4d1bef6518b8afb59f72071d3a4125
avgalaida <evaAxelrod@yandex.ru>
2024-11-08 13:33:28 +0500

    docs: add documentation for Views

--- Client/Views/MainWindow.xaml.cs
+++ Client/Views/MainWindow.xaml.cs
@@ -1,45 +1,104 @@
-﻿using System.Windows;
+﻿using Client.ViewModels;
+using System.Collections.Specialized;
+using System.Windows;
 using System.Windows.Input;
-using Client.ViewModels;
 
 namespace Client.Views;
+
+/// <summary>
+/// Основное окно приложения чата. Обрабатывает взаимодействие пользователя с интерфейсом и связывает его с логикой в ViewModel.
+/// </summary>
 public partial class MainWindow : Window
 {
-    private readonly MainViewModel _viewModel;
-
-    public MainWindow(MainViewModel viewModel)
+    /// <summary>
+    /// Инициализирует новый экземпляр класса <see cref="MainWindow"/>.
+    /// </summary>
+    public MainWindow()
     {
         InitializeComponent();
-
-        _viewModel = viewModel;
-        DataContext = _viewModel;
-
-        this.Closing += MainWindow_Closing;
-        this.Loaded += MainWindow_Loaded;
+        DataContextChanged += MainWindow_DataContextChanged;
     }
 
-    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    /// <summary>
+    /// Обработчик изменения контекста данных (DataContext).
+    /// Подписывается на события коллекции сообщений в ViewModel.
+    /// </summary>
+    /// <param name="sender">Источник события.</param>
+    /// <param name="e">Аргументы события изменения свойства.</param>
+    private void MainWindow_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
     {
-        //await _viewModel.ConnectAsync();
-    }
-
-    private async void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-    {
-        if (_viewModel.IsConnected)
+        if (e.OldValue is ChatViewModel oldViewModel)
         {
-            await _viewModel.DisconnectAsync();
+            // Отписка от событий старого ViewModel для предотвращения утечек памяти
+            oldViewModel.Messages.CollectionChanged -= Messages_CollectionChanged;
+            oldViewModel.SendMessageCommand.CanExecuteChanged -= SendMessageCommand_CanExecuteChanged;
+        }
+
+        if (DataContext is ChatViewModel viewModel)
+        {
+            // Подписка на изменения в коллекции Messages
+            viewModel.Messages.CollectionChanged += Messages_CollectionChanged;
+
+            // Подписка на изменения состояния команды SendMessageCommand
+            viewModel.SendMessageCommand.CanExecuteChanged += SendMessageCommand_CanExecuteChanged;
         }
     }
 
+    /// <summary>
+    /// Обработчик события изменения коллекции сообщений. Автоматически прокручивает список сообщений при добавлении новых.
+    /// </summary>
+    /// <param name="sender">Источник события.</param>
+    /// <param name="e">Аргументы события изменения коллекции.</param>
+    private void Messages_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (MessagesListBox != null)
+        {
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    if (e.NewStartingIndex == 0)
+                    {
+                        // Прокрутка вверх при подгрузке истории
+                        MessagesListBox.ScrollIntoView(MessagesListBox.Items[0]);
+                    }
+                    else if (e.NewStartingIndex == MessagesListBox.Items.Count - 1)
+                    {
+                        // Прокрутка вниз при добавлении нового сообщения
+                        MessagesListBox.ScrollIntoView(MessagesListBox.Items[MessagesListBox.Items.Count - 1]);
+                    }
+                }
+            }), System.Windows.Threading.DispatcherPriority.Background);
+        }
+    }
+
+    /// <summary>
+    /// Обработчик события изменения состояния команды <see cref="ChatViewModel.SendMessageCommand"/>.
+    /// Обновляет интерфейс в зависимости от возможности выполнения команды.
+    /// </summary>
+    /// <param name="sender">Источник события.</param>
+    /// <param name="e">Аргументы события.</param>
+    private void SendMessageCommand_CanExecuteChanged(object sender, EventArgs e)
+    {
+        // Здесь можно добавить логику, если требуется обновлять UI при изменении состояния команды
+    }
+
+    /// <summary>
+    /// Обработчик нажатия клавиш в текстовом поле для ввода сообщений.
+    /// Отправляет сообщение при нажатии клавиши Enter.
+    /// </summary>
+    /// <param name="sender">Источник события.</param>
+    /// <param name="e">Аргументы события нажатия клавиши.</param>
     private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter)
         {
-            if (_viewModel.SendMessageCommand.CanExecute(null))
+            if (DataContext is ChatViewModel viewModel && viewModel.SendMessageCommand.CanExecute(null))
             {
-                _viewModel.SendMessageCommand.Execute(null);
-                e.Handled = true;
+                viewModel.SendMessageCommand.Execute(null);
             }
+
+            e.Handled = true;
         }
     }
 }
